ecb1dae35b209ca6711d726369c687d4caad48a0
Imran <imrantipu90@yahoo.com>
2023-01-12 10:40:15 +0100

    database connection with user name and password

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb connection
+const uri =` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z2xprxb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 
 async function run()
