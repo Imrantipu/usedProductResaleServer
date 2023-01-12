e942da9301d4d86614a6847386342384ef7e011f
Imran <imrantipu90@yahoo.com>
2023-01-12 10:45:30 +0100

    homepage category product data load from mongodb

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb connection
-const uri =` mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z2xprxb.mongodb.net/?retryWrites=true&w=majority`;
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.z2xprxb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
@@ -20,6 +20,16 @@ async function run()
 {
   try
   {
+
+    const productsCollection = client.db('mobileHunt').collection('productsItem');
+
+// homepage category product collection api
+    app.get('/products', async (req, res) => {
+        const query = {}
+        const cursor = productsCollection.find(query);
+        const products = await cursor.toArray();
+        res.send(products);
+    });
     
   }
   finally{
